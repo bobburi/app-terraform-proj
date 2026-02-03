3215969033c71285f0beb1febd6fce1148764771
RAJESH BOBBURI <43173667+bobburi@users.noreply.github.com>
2026-02-03 09:52:39 +0530

    added new changes

--- vpc.tf
+++ vpc.tf
@@ -2,3 +2,9 @@ resource "google_compute_network" "vpc-123" {
     name = var.vpc_name
     auto_create_subnetworks = var.auto_create_subnetworks
 }
+
+resource "google_compute_subnetwork" "test-sub" {
+  name          = "tesrt-sub120"
+  ip_cidr_range = "10.10.0.0/24"
+  network       = google_compute_network.vpc-123.self_link
+}
